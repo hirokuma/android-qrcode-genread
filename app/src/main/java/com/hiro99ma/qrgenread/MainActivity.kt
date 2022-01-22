47a82c59b9d0f945d1aa9d8a82c9f22e2fb42d51
hirokuma <hiro9ma@gmail.com>
2022-01-22 17:59:56 +0900

    first commit

---
+++ app/src/main/java/com/hiro99ma/qrgenread/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.hiro99ma.qrgenread
+
+import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.ClipboardManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Point
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.CaptureActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
+
+//import com.google.zxing.integration
+
+class MainActivity : AppCompatActivity() {
+    class MyCaptureActivity : CaptureActivity()
+    private var screenWidth: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        val realSize = Point()
+        wm.defaultDisplay.getRealSize(realSize)
+        screenWidth = realSize.x
+    }
+
+    // Register the launcher and result handler
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents != null) {
+            // https://developer.android.com/guide/topics/text/copy-paste#Copying
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("qrcode", result.contents)
+            clipboard.setPrimaryClip(clip)
+
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = result.contents
+
+            Toast.makeText(this, "copy text to clipboard!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "canceled!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun onClickStartCamera(view: View) {
+        // https://qiita.com/tktktks10/items/3b327b2900d38e672996
+        val options = ScanOptions().apply {
+            captureActivity = MyCaptureActivity::class.java
+        }
+        barcodeLauncher.launch(options)
+    }
+
+    fun onClickReadClipboard(view: View) {
+        // https://developer.android.com/guide/topics/text/copy-paste#Pasting
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        if (clipboard.hasPrimaryClip() && clipboard.primaryClipDescription!!.hasMimeType(
+                MIMETYPE_TEXT_PLAIN
+            )
+        ) {
+            val text = clipboard.primaryClip!!.getItemAt(0).text
+
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = text
+
+            // https://stackoverflow.com/questions/19337448/generate-qr-code-directly-into-imageview
+            val matrix = MultiFormatWriter().encode(
+                text.toString(),
+                BarcodeFormat.QR_CODE, screenWidth, screenWidth
+            )
+            val height: Int = matrix.height
+            val width: Int = matrix.width
+            val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bmp.setPixel(x, y, if (matrix.get(x, y)) Color.BLACK else Color.WHITE)
+                }
+            }
+            val imageView = findViewById<ImageView>(R.id.imageView) as ImageView
+            imageView.setImageBitmap(bmp)
+        }
+    }
+}
